53d09f89325e6672580f3d66cb1db302adaeda57
Nada Nabil <nadanabil2307@gmail.com>
2024-04-15 05:45:15 +0200

    - handle di

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_mentorship_b1/core/di/dependency_injection.dart';
 
 import 'core/routing/app_router.dart';
 import 'my_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  setupGetIt();
 
   runApp(MyApp(
     appRouter: AppRouter(),
