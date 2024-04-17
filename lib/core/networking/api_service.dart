c29ad983a6c87e5a78733712c7e768b94873327a
Nada Nabil <nadanabil2307@gmail.com>
2024-04-17 22:46:40 +0200

    - fixed api service file

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -9,7 +9,7 @@ class ApiService {
 
   ApiService(this._dio);
 
-  Future<Map<String, dynamic>> get({required String endPoint}) async {
+  Future<dynamic> get({required String endPoint}) async {
     var response = await _dio.get('$baseUrl$endPoint');
     return response.data;
   }
