15d42ca4b3af5b754bd5180f2e26accb574a68a0
Nada Nabil <nadanabil2307@gmail.com>
2024-04-17 23:13:52 +0200

    - added rocket cubit and repo in di

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_mentorship_b1/features/rockets/data/repo/all_rockets_repo.dart';
+import 'package:flutter_mentorship_b1/features/rockets/logic/all_rockets_cubit.dart';
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 
@@ -9,4 +11,8 @@ final getIt = GetIt.instance;
 Future<void> setupGetIt() async {
   Dio dio = DioFactory.getDio();
   getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+  getIt.registerLazySingleton<AllRocketsRepo>(
+      () => AllRocketsRepo(apiService: getIt()));
+  getIt.registerLazySingleton<AllRocketsCubit>(
+      () => AllRocketsCubit(allRocketsRepo: getIt()));
 }
