5dae31cfb9d883c294896737fe38151d00eaa72a
Nada Nabil <nadanabil2307@gmail.com>
2024-04-30 22:40:32 +0300

    - added auth cubit as bloc provider

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_mentorship_b1/features/authentication/logic/auth/auth_cubit.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/constants/app_strings.dart';
+import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 import 'core/routing/routes.dart';
 import 'core/themes/theme_manager.dart';
@@ -17,12 +20,15 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 667),
       minTextAdapt: true,
       builder: (context, widget) {
-        return MaterialApp(
-          title: AppStrings.appName,
-          theme: getApplicationTheme(),
-          debugShowCheckedModeBanner: false,
-          initialRoute: Routes.splash,
-          onGenerateRoute: appRouter.generateRoute,
+        return BlocProvider(
+          create: (context) => getIt<AuthCubit>()..getUser(),
+          child: MaterialApp(
+            title: AppStrings.appName,
+            theme: getApplicationTheme(),
+            debugShowCheckedModeBanner: false,
+            initialRoute: Routes.splash,
+            onGenerateRoute: appRouter.generateRoute,
+          ),
         );
       },
     );
