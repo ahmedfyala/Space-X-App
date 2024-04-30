719cd7d60686986706e2b9747ccb5397469d3987
Nada Nabil <nadanabil2307@gmail.com>
2024-04-30 22:37:30 +0300

    - added login cubit

---
+++ lib/features/authentication/logic/login/login_cubit.dart
@@ -0,0 +1,37 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import '../../data/repo/auth_repo.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final AuthRepository userRepository;
+
+  LoginCubit(this.userRepository) : super(LoginInitial());
+
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
+
+  Future<void> login() async {
+    if (formKey.currentState!.validate()) {
+      emit(LoginLoading());
+      var response = await userRepository.login(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      response.fold(
+        (failure) {
+          log(failure.message);
+          emit(LoginFailure(failure.message));
+        },
+        (user) {
+          log(user.toString());
+          emit(LoginSuccess());
+        },
+      );
+    }
+  }
+}
