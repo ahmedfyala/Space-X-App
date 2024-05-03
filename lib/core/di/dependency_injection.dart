187b1e139e3d4f4ae4acae575d988100bc00473d
Nada Nabil <nadanabil2307@gmail.com>
2024-05-03 18:39:09 +0300

    - handled Ui while loading in login and register - edited git it type of auth cubits

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -52,13 +52,13 @@ Future<void> setupGetIt() async {
   // auth
   getIt.registerLazySingleton<AuthRepository>(() => AuthRepository());
 
-  getIt.registerLazySingleton<RegisterCubit>(() => RegisterCubit(getIt()));
-  getIt.registerLazySingleton<AuthCubit>(() => AuthCubit(getIt()));
+  getIt.registerFactory<RegisterCubit>(() => RegisterCubit(getIt()));
+  getIt.registerFactory<AuthCubit>(() => AuthCubit(getIt()));
 
-  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
 }
 
-Future<void> initAuthFeature()async{
+Future<void> initAuthFeature() async {
   getIt.registerFactory<AuthRepository>(() => AuthRepository());
 
   getIt.registerFactory<RegisterCubit>(() => RegisterCubit(getIt()));
