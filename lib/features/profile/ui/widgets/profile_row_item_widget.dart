06097ff771c5075f597ec70c9b81da6b9e7a2955
Nada Nabil <nadanabil2307@gmail.com>
2024-05-19 23:05:37 +0300

    added profile row item widget that show icon, title, and arrow icon

---
+++ lib/features/profile/ui/widgets/profile_row_item_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/helpers/spaces.dart';
+import '../../../../core/themes/styles.dart';
+
+class ProfileRowItemWidget extends StatelessWidget {
+  final String title;
+  final String routeName;
+  final IconData icon;
+  final Function()? onTap;
+  const ProfileRowItemWidget({
+    super.key,
+    required this.routeName,
+    required this.title,
+    required this.icon,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InkWell(
+          onTap: onTap ??
+              () {
+                // context.pushNamed(routeName);
+              },
+          child: Row(
+            children: [
+              Icon(
+                icon,
+              ),
+              horizontalSpace(10.h),
+              Text(
+                title,
+                style: TextStyles.font16Regular,
+              ),
+              const Spacer(),
+              const Icon(Icons.redo_rounded),
+            ],
+          ),
+        ),
+        verticalSpace(10.h),
+      ],
+    );
+  }
+}
