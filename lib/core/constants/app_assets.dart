76f7b4584e359c3a9255a2cdb2c19fbc36e371b0
Nada Nabil <nadanabil2307@gmail.com>
2024-05-24 16:55:14 +0300

    added company strings and images

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -6,12 +6,20 @@ class AssetsPaths {
   static const String lottie = '$assets/lottie';
 }
 
-class AppImages {}
+class AppImages {
+  static const String user =
+      'https://llllline.com/images/thumbs/0000038270_eps_800.jpeg';
+
+  static const String cooImage =
+      'https://www.elonx.net/wp-content/uploads/gwynne_shotwell_soace_foundation-1280x720.jpg';
+  static const String founderImage =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXK27qC3H2toKeNwIQHDO3IiW35bo-1UX59g&s';
+  static const String spaceXLogo =
+      'https://www.spacex.com/static/images/share.jpg';
+}
 
 class AppPng {
   static const String rocket = '${AssetsPaths.png}/rocket.png';
-  static const String user =
-      'https://llllline.com/images/thumbs/0000038270_eps_800.jpeg';
 }
 
 class AppSvgs {}
