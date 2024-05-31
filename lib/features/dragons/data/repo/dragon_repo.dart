76410ee772a8b82ca6cecd8e81e64bd92ad03dc4
Nada Nabil <nadanabil2307@gmail.com>
2024-06-01 01:01:20 +0300

    added dragons repo

---
+++ lib/features/dragons/data/repo/dragon_repo.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_mentorship_b1/core/helpers/extensions.dart';
+import 'package:flutter_mentorship_b1/core/networking/api_constants.dart';
+import 'package:flutter_mentorship_b1/core/networking/error_handler/error_handler.dart';
+import 'package:flutter_mentorship_b1/core/networking/error_handler/failure.dart';
+import 'package:flutter_mentorship_b1/core/networking/network_info/network_info.dart';
+import 'package:flutter_mentorship_b1/core/networking/api_service.dart';
+import 'package:flutter_mentorship_b1/features/dragons/data/models/dragon_model.dart';
+import '../../../../core/helpers/enums.dart';
+
+class DragonsRepo {
+  final ApiService apiService;
+  final NetworkInfo networkInfo;
+
+  DragonsRepo({required this.apiService, required this.networkInfo});
+
+  Future<Either<Failure, List<Dragon>>> fetchAllDragons() async {
+    if (await networkInfo.isConnected) {
+      try {
+        var response = await apiService.get(endPoint: ApiUrls.dragons);
+
+        List<Dragon> dragons = [];
+        response.forEach((element) {
+          dragons.add(Dragon.fromJson(element));
+        });
+        return right(dragons);
+      } catch (error) {
+        return left(ErrorHandler.handel(error).failure);
+      }
+    } else {
+      return Left(HttpStatusCodeEnum.noInternetConnection.getFailure());
+    }
+  }
+}
