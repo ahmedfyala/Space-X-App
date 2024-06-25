bc7c80d0c104ab57791766326f6219c38467d762
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-06-25 23:40:30 +0300

    edited category model

--- lib/features/space/models/category_model.dart
+++ lib/features/space/models/category_model.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class CategoryModel {
-  final IconData icon;
+  final String image;
   final String text;
   final String routeName;
 
   CategoryModel({
-    required this.icon,
+    required this.image,
     required this.text,
     required this.routeName,
   });
