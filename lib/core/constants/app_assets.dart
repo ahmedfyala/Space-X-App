ab17dc62efb7eab6fe3534f7bb64d4ca61024fba
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-06-25 23:39:50 +0300

    added categories images

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -16,6 +16,16 @@ class AppImages {
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXK27qC3H2toKeNwIQHDO3IiW35bo-1UX59g&s';
   static const String spaceXLogo =
       'https://www.spacex.com/static/images/share.jpg';
+  static const String rocketCategory =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUJ8s2eh6mgbVE_pnhD6KtftZs9eURQAQST8joqZeVF9zugOA-222zMvA_ubquVq9Qiog&usqp=CAU';
+  static const String shipsCategory =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVDMb8UEAPkbE8wC_5XpDuDMScuTplpIF9Kg8oeX_1j2297k37-uQRloOiYLZM6iOnCv0&usqp=CAU';
+  static const String dragonsCategory =
+      'https://static.vecteezy.com/system/resources/previews/020/122/559/original/black-silhouette-of-rocket-launch-free-vector.jpg';
+  static const String launchesCategory =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLXe1P3LdSK73PQMvZIo6fXOXBrGSPEKi3EQ&s';
+  static const String homeImage =
+      'https://mrwallpaper.com/images/hd/astronaut-aesthetic-with-sunflowers-97v57ltgcfkqicpn.jpg';
 }
 
 class AppPng {
